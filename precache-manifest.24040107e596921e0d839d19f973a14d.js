fd0168629cbc75c378d48fd1475d65c113a45b51
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-07-31 16:29:15 +0200

    Updates

---
+++ precache-manifest.24040107e596921e0d839d19f973a14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08cde1a37ec1b4313c64194a48651e0",
+    "url": "/E-com-WebShop/index.html"
+  },
+  {
+    "revision": "28aa566870d3736aa55a",
+    "url": "/E-com-WebShop/static/css/main.91aeb6fa.chunk.css"
+  },
+  {
+    "revision": "9e141cbb2bf571ffaac8",
+    "url": "/E-com-WebShop/static/js/2.e103ea1b.chunk.js"
+  },
+  {
+    "revision": "28aa566870d3736aa55a",
+    "url": "/E-com-WebShop/static/js/main.4366ad0a.chunk.js"
+  },
+  {
+    "revision": "63a78eb15c540932d03d",
+    "url": "/E-com-WebShop/static/js/runtime~main.c9b2d01e.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/E-com-WebShop/static/media/11.2 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/E-com-WebShop/static/media/4.4 crown.svg.ed28accb.svg"
+  }
+]);
